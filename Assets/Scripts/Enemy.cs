a2bd9ae7817e7a5a77a06ce30882ed7bfdd2838d
dachoa1995 <bui@ryukyu-i.co.jp>
2020-08-14 10:53:51 +0900

    Build basic Laser Defender feature

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShot = 0.2f;
+    [SerializeField] float maxTimeBetweenShot = 3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
+    private void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShot, maxTimeBetweenShot);
+    }
+
+    private void Update()
+    {
+        CountDownAndShot();
+    }
+
+    private void CountDownAndShot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShot, maxTimeBetweenShot);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject lazer = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        lazer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if(!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.getDamage();
+        damageDealer.Hit();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Way Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefabs;
+    [SerializeField] GameObject pathPrefabs;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 10;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject getEnemyPrefabs() { return enemyPrefabs; }
+
+    public List<Transform> getWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach(Transform child in pathPrefabs.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+
+    public float getTimeBetweenSpawns() { return timeBetweenSpawns; }
+
+    public float getSpawnRandomFactor() { return spawnRandomFactor; }
+
+    public int getNumberOfEnemies() { return numberOfEnemies; }
+
+    public float getMoveSpeed() { return moveSpeed; }
+}
